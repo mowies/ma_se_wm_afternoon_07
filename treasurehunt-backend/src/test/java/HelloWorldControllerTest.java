0aa2e5d339cea4cd6352e99092ce7aa9d8fd7682
Robert Tieber <robert.tieber@student.tugraz.at>
2018-04-11 16:10:01 +0200

    Fixed hello world test

--- treasurehunt-backend/src/test/java/HelloWorldControllerTest.java
+++ treasurehunt-backend/src/test/java/HelloWorldControllerTest.java
@@ -1,20 +1,21 @@
+import com.geoschnitzel.treasurehunt.backend.HelloWorldController;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
-@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = HelloWorldController.class)
 public class HelloWorldControllerTest {
 
     private MockMvc mvc;
@@ -29,10 +30,10 @@ public class HelloWorldControllerTest {
 
     @Test
     public void testCreationOfANewProjectSucceeds() throws Exception {
-        mvc.perform(get("/helloWorld")).andExpect(result -> {
-            String response = new String(result.getResponse().getContentAsByteArray());
-            assertEquals("Hello World!", response);
-        });
+        String helloWorldResult = mvc.perform(get("/helloWorld").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+        assertEquals("Hello World!", helloWorldResult);
     }
 
 }
