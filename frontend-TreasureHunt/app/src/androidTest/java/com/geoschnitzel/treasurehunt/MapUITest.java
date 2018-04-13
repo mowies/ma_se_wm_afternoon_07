77811814f1e652c52d937204e9a99240c2b4e303
Robert Tieber <robert.tieber@student.tugraz.at>
2018-04-13 16:40:41 +0200

    added navigation drawer and burger menu button to MapActivity

--- frontend-TreasureHunt/app/src/androidTest/java/com/geoschnitzel/treasurehunt/MapUITest.java
+++ frontend-TreasureHunt/app/src/androidTest/java/com/geoschnitzel/treasurehunt/MapUITest.java
@@ -1,24 +1,36 @@
 package com.geoschnitzel.treasurehunt;
 
+import android.support.test.espresso.contrib.DrawerActions;
 import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.geoschnitzel.treasurehunt.map.MapActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class MapUITest {
+    @Rule
+    public ActivityTestRule<MapActivity> mTasksActivityTestRule =
+            new ActivityTestRule<MapActivity>(MapActivity.class) {
+
+            };
 
     @Test
     public void openNavDrawer_showsNavDrawer() {
-        onView(withId(R.id.burger_menu)).perform(click());
+        onView(withId(R.id.drawer_layout))
+//                .check(matches(isClosed(Gravity.LEFT))) // Left Drawer should be closed.
+                .perform(DrawerActions.open()); // Open Drawer
 
         onView(withId(R.id.nav_view)).check(matches(isDisplayed()));
     }
