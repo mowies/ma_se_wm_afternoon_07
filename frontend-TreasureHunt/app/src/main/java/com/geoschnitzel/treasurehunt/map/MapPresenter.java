fd608cdccfa10357727e2e06f3d744eaf522c165
Robert Tieber <robert.tieber@student.tugraz.at>
2018-04-13 15:30:59 +0200

    Updated gradle and added first test for MVP architecture

---
+++ frontend-TreasureHunt/app/src/main/java/com/geoschnitzel/treasurehunt/map/MapPresenter.java
@@ -0,0 +1,12 @@
+package com.geoschnitzel.treasurehunt.map;
+
+public class MapPresenter implements MapContract.Presenter {
+    public MapPresenter(MapContract.View mMapView) {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+}
