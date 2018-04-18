73379dc50d608bda2675837c20c5f25ec547b9e0
Robert Tieber <robert.tieber@student.tugraz.at>
2018-04-18 15:04:51 +0200

    Added empty shared module

---
+++ shared-treasurehunt/build.gradle.kts
@@ -0,0 +1,18 @@
+group = "com.geoschnitzel"
+version = "1.0-SNAPSHOT"
+
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testCompile("junit", "junit", "4.12")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
