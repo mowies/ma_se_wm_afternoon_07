4643189c762605c7af186d0ea884ebb4ce33b6d0
Patrick Schwarz <patrick.schwarz@student.tugraz.at>
2018-04-18 17:29:16 +0200

    New Architecture for drawer and top bar in BaseActivity

--- frontend-treasurehunt/src/main/java/com/geoschnitzel/treasurehunt/shpurchase/SHPurchaseActivity.java
+++ frontend-treasurehunt/src/main/java/com/geoschnitzel/treasurehunt/shpurchase/SHPurchaseActivity.java
@@ -2,18 +2,17 @@ package com.geoschnitzel.treasurehunt.shpurchase;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.widget.GridView;
 
 import com.geoschnitzel.treasurehunt.R;
+import com.geoschnitzel.treasurehunt.base.BaseActivity;
 import com.geoschnitzel.treasurehunt.model.SHPurchaseItem;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class SHPurchaseActivity extends AppCompatActivity {
+public class SHPurchaseActivity extends BaseActivity {
 
 
     @Override
@@ -61,15 +60,8 @@ public class SHPurchaseActivity extends AppCompatActivity {
                 .setSHValue(5000)
                 .setPrice(24.99f));
 
-        glshpurchase.setAdapter(new SHPurchaseAdapter(testList, this.getApplicationContext()));
+        glshpurchase.setAdapter(new SHPurchaseAdapter(testList,this.getApplicationContext()));
 
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
