d41f280545ba3388b096943c8a79025a80ba6068
Robert Tieber <robert.tieber@student.tugraz.at>
2018-04-25 15:17:03 +0200

    Fixed test configuration

--- backend-treasurehunt/src/test/java/com/geoschnitzel/treasurehunt/backend/HelloWorldControllerTest.java
+++ backend-treasurehunt/src/test/java/com/geoschnitzel/treasurehunt/backend/HelloWorldControllerTest.java
@@ -11,7 +11,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -27,8 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {HelloWorldController.class, HelloDatabaseController.class})
-@EnableWebMvc
+@SpringBootTest
 public class HelloWorldControllerTest {
 
     @MockBean
@@ -55,7 +53,7 @@ public class HelloWorldControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().json("{\n" +
                         "  \"message\": \"Hello World\",\n" +
-                        "  \"timestamp\": \"2018-04-18T15:22:13.804+0000\"\n" +
+                        "  \"timestamp\": \"2018-04-18T15:43:20.885+0000\"\n" +
                         "}"));
     }
 
