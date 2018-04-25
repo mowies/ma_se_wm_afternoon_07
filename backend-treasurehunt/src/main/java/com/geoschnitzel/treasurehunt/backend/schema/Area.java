b8f129aa13e7c0b2553d3a5d3446e4230de3697e
Robert Tieber <robert.tieber@student.tugraz.at>
2018-04-25 14:06:34 +0200

    Created schema for schnitzelhunt game

--- backend-treasurehunt/src/main/java/com/geoschnitzel/treasurehunt/backend/schema/Area.java
+++ backend-treasurehunt/src/main/java/com/geoschnitzel/treasurehunt/backend/schema/Area.java
@@ -1,6 +1,7 @@
 package com.geoschnitzel.treasurehunt.backend.schema;
 
 import javax.persistence.Embeddable;
+import javax.persistence.Embedded;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,9 +13,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Area {
 
-    private double latitude;
-    private double longitude;
+    public Area(double latitude, double longitude, int radius) {
+        this.coordinate = new Coordinate(latitude, longitude);
+        this.radius = radius;
+    }
 
+    @Embedded
+    private Coordinate coordinate;
     /**
      * Radius in meters.
      * Shouldn't be too small
