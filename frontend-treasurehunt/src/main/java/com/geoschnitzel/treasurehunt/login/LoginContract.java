2894c50aa344652d59e599a0cf89cee159f2da62
Simon Posch <simon.posch@student.tugraz.at>
2018-04-29 11:24:46 +0200

    removed unnecessary imports

--- frontend-treasurehunt/src/main/java/com/geoschnitzel/treasurehunt/login/LoginContract.java
+++ frontend-treasurehunt/src/main/java/com/geoschnitzel/treasurehunt/login/LoginContract.java
@@ -2,9 +2,6 @@ package com.geoschnitzel.treasurehunt.login;
 
 import com.geoschnitzel.treasurehunt.IBasePresenter;
 import com.geoschnitzel.treasurehunt.IBaseView;
-import com.geoschnitzel.treasurehunt.rest.SHPurchaseItem;
-
-import java.util.List;
 
 public interface LoginContract {
 
