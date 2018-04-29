9257a8bdc8ff608b229ca50bc581549ee369b8a8
Simon Posch <simon.posch@student.tugraz.at>
2018-04-29 11:18:49 +0200

    changed login basename

---
+++ frontend-treasurehunt/src/main/java/com/geoschnitzel/treasurehunt/login/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.geoschnitzel.treasurehunt.login;
+
+import android.os.Bundle;
+
+import com.geoschnitzel.treasurehunt.R;
+import com.geoschnitzel.treasurehunt.base.BaseActivityWithBackButton;
+import com.geoschnitzel.treasurehunt.utils.ActivityUtils;
+
+public class LoginActivity extends BaseActivityWithBackButton {
+
+    LoginContract.Presenter mPresenter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        LoginFragment loginFragment =
+                (LoginFragment) getSupportFragmentManager().findFragmentById(R.id.contentFrame);
+        if (loginFragment == null) {
+            // Create the fragment
+            loginFragment = LoginFragment.newInstance();
+            ActivityUtils.addFragmentToActivity(
+                    getSupportFragmentManager(), loginFragment, R.id.contentFrame);
+        }
+
+        mPresenter = new LoginPresenter(loginFragment);
+    }
+
+    @Override
+    public void onBackPressed() {
+        this.finishAffinity();
+    }
+}
---
+++ frontend-treasurehunt/src/main/java/com/geoschnitzel/treasurehunt/login/LoginContract.java
@@ -0,0 +1,19 @@
+package com.geoschnitzel.treasurehunt.login;
+
+import com.geoschnitzel.treasurehunt.IBasePresenter;
+import com.geoschnitzel.treasurehunt.IBaseView;
+import com.geoschnitzel.treasurehunt.rest.SHPurchaseItem;
+
+import java.util.List;
+
+public interface LoginContract {
+
+    interface View extends IBaseView<Presenter> {
+
+    }
+
+    interface Presenter extends IBasePresenter {
+
+    }
+
+}
