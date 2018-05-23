dc7485bfb893d91590981aeea5c1b92fb4037b61
Robert Tieber <robert.tieber@student.tugraz.at>
2018-05-23 15:26:14 +0200

    [RT, SP, PS] Fixed test data generation

--- backend-treasurehunt/src/main/java/com/geoschnitzel/treasurehunt/backend/service/TestDataService.java
+++ backend-treasurehunt/src/main/java/com/geoschnitzel/treasurehunt/backend/service/TestDataService.java
@@ -48,14 +48,12 @@ public class TestDataService {
     @Transactional
     @EventListener(ApplicationReadyEvent.class)
     public void generateTestData() {
-        if (userRepository.count() >= 0) {
+        if (userRepository.count() > 0) {
             return;
         }
 
         List<User> users = generateUsers();
         userRepository.saveAll(users);
-        System.out.println("Users: " + userRepository.count());
-        System.out.println("Generated users: " + users.size());
         List<Hunt> hunts = generateSchnitzelHunts(users.get(1));
         huntRepository.saveAll(hunts);
         gameRepository.save(generateGame(users.get(0), hunts.get(0)));
