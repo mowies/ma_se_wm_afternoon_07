37bd9d3c7dfbd83fc6c75d1edd8ddfa38069fb4a
Robert Tieber <robert.tieber@gmail.com>
2018-05-24 20:22:01 +0200

    [RT, SP, PS] Made server start much simpler (and possibly faster) (#65)

--- run_server.sh
+++ run_server.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-./gradlew :backend-treasurehunt:build
-cd backend-treasurehunt/build/distributions
-
-zip_file=backend-treasurehunt-boot-*.zip
-folder=`basename ${zip_file} .zip`
-rm -rf ${folder}
-unzip ${zip_file}
-
-echo "Folder is $folder"
-cd ${folder}/bin
-./backend-treasurehunt
+./gradlew :backend-treasurehunt:bootRun
