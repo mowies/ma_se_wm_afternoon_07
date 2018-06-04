6d65c5475194b85be9b9ac4199b34ea2fb83b23e
Simon Posch <simon.posch@student.tugraz.at>
2018-06-04 16:02:15 +0200

    [MG, SP] added clicklistener

--- frontend-treasurehunt/src/main/java/com/geoschnitzel/treasurehunt/shcreatehunt/SHCreateFragment.java
+++ frontend-treasurehunt/src/main/java/com/geoschnitzel/treasurehunt/shcreatehunt/SHCreateFragment.java
@@ -2,10 +2,12 @@ package com.geoschnitzel.treasurehunt.shcreatehunt;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.geoschnitzel.treasurehunt.R;
@@ -22,6 +24,33 @@ public class SHCreateFragment extends Fragment implements SHCreateContract.View 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_shcreatehunt, container, false);
         ImageButton image = (ImageButton) root.findViewById(R.id.new_hunt_image);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                }
+        });
+
+        Button forward = (Button) root.findViewById(R.id.new_hunt_next);
+        forward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Button abort = (Button) root.findViewById(R.id.new_hunt_abort);
+        abort.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
         return root;
     }
 
